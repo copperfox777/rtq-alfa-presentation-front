c1df29e8d6eea7c304f9c5bd3addccf7f06194f9
Степанов Антон Сергеевич <AStepanov12@alfabank.ru>
2022-05-26 12:55:47 +0300

    add poling interval for posts

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,38 +1,46 @@
-import {useParams} from "react-router-dom";
-import {useGetPostsQuery, useGetUserQuery} from "../redux/api";
-import {PostCard} from "../components/Views/PostCard";
-import {Error} from "../components/Views/Error";
-import {Loading} from "../components/Views/Loading";
-import {Title} from "../components/Views/Title";
-import {User} from "../components/User";
-import {useEffect} from "react";
-import {Button} from "../components/Views/Button";
+import { useParams } from "react-router-dom";
+import { useGetPostsQuery, useGetUserQuery } from "../redux/api";
+import { PostCard } from "../components/Views/PostCard";
+import { Error } from "../components/Views/Error";
+import { Loading } from "../components/Views/Loading";
+import { Title } from "../components/Views/Title";
+import { User } from "../components/User";
+import { useEffect } from "react";
+import { Button } from "../components/Views/Button";
+import cl from "classnames";
 
 export function UserPage() {
-
   const { id } = useParams();
-  const {data: posts, isError, isFetching,refetch:refetchPosts} = useGetPostsQuery(+id);
-  const {refetch:refetchUser} = useGetUserQuery(+id)
-    useEffect(() => {
-        if (isError) refetchPosts();
-    }, [isError]);
+  const {
+    data: posts,
+    isError,
+    isLoading,
+    isFetching,
+    refetch: refetchPosts,
+  } = useGetPostsQuery(+id, { pollingInterval: 3000 });
+
+  const { refetch: refetchUser } = useGetUserQuery(+id);
+
+  useEffect(() => {
+    if (isError) refetchPosts();
+  }, [isError]);
 
   return (
     <div className="m-10">
       <div className="flex gap-10 justify-end mb-10 items-center">
-          <Button title="Refetch User" onClick={refetchUser}/>
-          <Button title="Refetch Posts" onClick={refetchPosts}/>
+        <Button title="Refetch User" onClick={refetchUser} />
+        <Button title="Refetch Posts" onClick={refetchPosts} />
         <Title text="User" />
       </div>
-      <User id={id}/>
+      <User id={id} />
       <div className="my-10">
-        {isFetching && <Loading/>}
-        {isError && <Error/>}
-        {!isFetching &&
-            !isError &&
-            posts?.map((post) => (
-                <PostCard key={post.id} post={post}/>
-            ))}
+        {isLoading && <Loading />}
+        {isError && <Error />}
+        <div className={cl(isFetching && "opacity-50")}>
+          {posts?.map((post) => (
+            <PostCard key={post.id} post={post} />
+          ))}
+        </div>
       </div>
     </div>
   );
