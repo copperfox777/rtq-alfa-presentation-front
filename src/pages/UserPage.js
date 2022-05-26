14734615302801a47476dd710b9accd24396c182
Степанов Антон Сергеевич <AStepanov12@alfabank.ru>
2022-05-26 12:48:14 +0300

    add refetch buttons

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,24 +1,27 @@
 import {useParams} from "react-router-dom";
-import {useGetPostsQuery} from "../redux/api";
+import {useGetPostsQuery, useGetUserQuery} from "../redux/api";
 import {PostCard} from "../components/Views/PostCard";
 import {Error} from "../components/Views/Error";
 import {Loading} from "../components/Views/Loading";
 import {Title} from "../components/Views/Title";
 import {User} from "../components/User";
 import {useEffect} from "react";
+import {Button} from "../components/Views/Button";
 
 export function UserPage() {
 
   const { id } = useParams();
-  const {data: posts, isError, isFetching,refetch} = useGetPostsQuery(+id);
-
+  const {data: posts, isError, isFetching,refetch:refetchPosts} = useGetPostsQuery(+id);
+  const {refetch:refetchUser} = useGetUserQuery(+id)
     useEffect(() => {
-        if (isError) refetch();
+        if (isError) refetchPosts();
     }, [isError]);
 
   return (
     <div className="m-10">
       <div className="flex gap-10 justify-end mb-10 items-center">
+          <Button title="Refetch User" onClick={refetchUser}/>
+          <Button title="Refetch Posts" onClick={refetchPosts}/>
         <Title text="User" />
       </div>
       <User id={id}/>
