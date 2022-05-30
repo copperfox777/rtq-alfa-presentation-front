b8bbb5a9d7745c854251c6f7fd37076ba064d48a
Степанов Антон Сергеевич <AStepanov12@alfabank.ru>
2022-05-30 14:37:59 +0300

    mutation fetchBaseQuery

--- src/redux/api.js
+++ src/redux/api.js
@@ -13,20 +13,11 @@ export const api = createApi({
       providesTags: (result, error, arg) => [{type: "Posts", id: arg}],
     }),
     updateUser: builder.mutation({
-      queryFn: async (data) => {
-        const {id, ...body} = data;
-        try {
-          let response = await fetch(`http://localhost:7070/api/users/${id}`, {
-            method: "post",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(body),
-          });
-          if (response.ok) return {data: null};
-          return {error: "Something bad happened"};
-        } catch (e) {
-          return {error: "Something bad happened"};
-        }
-      },
+      query: ({id, ...body}) => ({
+        url: `users/${id}`,
+        method: 'POST',
+        body,
+      }),
       invalidatesTags: (result, error, {id}) => [{type: "Users", id}],
     }),
   }),
